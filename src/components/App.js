70569f0b06c7827c659a4b6509536c2385a0ac13
zippykitche <zippyk80@gmail.com>
2024-11-22 00:37:37 +0300

    complete fetching lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, {useEffect, useState} from "react";
+
+function App(){
+    const [showImage, setShowImage] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch ("https://dog.ceo/api/breeds/image/random")
+        .then((r) => r.json())
+        .then((data) => {
+            setShowImage(data.message);
+            setIsLoaded(true);
+        });
+    }, []);
+
+    return (
+        <div>
+          {!isLoaded ? (
+            <p>Loading...</p>
+          ) : (
+            <img src={showImage} alt="A Random Dog" />
+          )}
+        </div>
+      );
+}
+
+export default App;
